7a2d927bc1c6a8d9f9834f351cb975a0adc81705
Phoque <lafont_jordan@hotmail.fr>
2021-12-22 11:24:03 +0100

    Player movement, safety net

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,21 +12,30 @@ public class PlayerController : NetworkBehaviour
 	[SerializeField]
 	Inputs inputs;
 
+	[Header("Components")]
+	[SerializeField]
+	Transform t;
+	[SerializeField]
+	Rigidbody rb;
+
 	[Header("Fields")]
 	[SerializeField]
 	float spawnXRange;
 	[SerializeField]
 	float spawnZRange;
+	[SerializeField]
+	float speed;
 
 	//Private Fields
+	GameObject playerListItem;
 	NetworkBehaviour sceneManager;
 	Transform playerListContainer;
-	GameObject playerListItem;
+	bool canMove = true;
 
 	private void Start()
 	{
 		NetworkManager.SceneManager.OnLoadComplete += SceneManager_OnLoadComplete;
-		transform.position = new Vector3(Random.Range(-spawnXRange, spawnXRange), 0f, Random.Range(-spawnZRange, spawnZRange));
+		t.position = new Vector3(Random.Range(-spawnXRange, spawnXRange), 4f, Random.Range(-spawnZRange, spawnZRange));
 		playerListContainer = GameObject.FindGameObjectWithTag("PlayerListContainer").transform;
 		playerListItem = Instantiate(playerListItemPrefab, playerListContainer);
 	}
@@ -36,22 +45,23 @@ public class PlayerController : NetworkBehaviour
 		if (sceneName == "Game")
 		{
 			sceneManager = GameObject.FindGameObjectWithTag("SceneManager").GetComponent<NetworkBehaviour>();
-			transform.position = ((GameScene)sceneManager).GetSpawnPoint().position;
+			t.position = ((GameScene)sceneManager).GetSpawnPoint().position;
 		}
 	}
 
 	private void Update()
 	{
-		if (IsClient)
+
+	}
+
+	private void FixedUpdate()
+	{
+		if (IsClient) //move
 		{
-			if (inputs.movement != Vector2.zero)
+			if (canMove && inputs.movement != Vector2.zero)
 			{
-
+				rb.velocity = new Vector3(inputs.movement.x * speed, rb.velocity.y, inputs.movement.y * speed);
 			}
-		}
-		if (IsServer)
-		{
-
 		}
 	}
 
