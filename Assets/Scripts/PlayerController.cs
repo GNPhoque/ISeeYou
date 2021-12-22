e481da277c30df8e9b7be5f47b02a42491083212
Phoque <lafont_jordan@hotmail.fr>
2021-12-22 12:15:40 +0100

    Added FPV cinemachine camera
    players set local camera to follow when scene is loaded
    players rotate towards camera forward when moving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -24,7 +25,9 @@ public class PlayerController : NetworkBehaviour
 	[SerializeField]
 	float spawnZRange;
 	[SerializeField]
-	float speed;
+	float moveSpeed;
+	[SerializeField]
+	float rotationSpeed;
 
 	//Private Fields
 	GameObject playerListItem;
@@ -46,23 +49,32 @@ public class PlayerController : NetworkBehaviour
 		{
 			sceneManager = GameObject.FindGameObjectWithTag("SceneManager").GetComponent<NetworkBehaviour>();
 			t.position = ((GameScene)sceneManager).GetSpawnPoint().position;
+			if (IsOwner) Camera.main.GetComponent<CinemachineVirtualCamera>().Follow = t;
 		}
-	}
-
-	private void Update()
-	{
-
 	}
 
 	private void FixedUpdate()
 	{
-		if (IsClient) //move
+		if (IsOwner) //move
 		{
 			if (canMove && inputs.movement != Vector2.zero)
 			{
-				rb.velocity = new Vector3(inputs.movement.x * speed, rb.velocity.y, inputs.movement.y * speed);
+				Move();
+
+				Vector3 cameraForward = Camera.main.transform.forward;
+				cameraForward = new Vector3(cameraForward.x, 0f, cameraForward.z);
+				rb.MoveRotation(Quaternion.RotateTowards(rb.rotation, Quaternion.LookRotation(cameraForward), rotationSpeed));
 			}
 		}
+	}
+
+	private void Move()
+	{
+		Vector3 moveX = t.right * inputs.movement.x * moveSpeed;
+		Vector3 moveZ = t.forward * inputs.movement.y * moveSpeed;
+		Vector3 move = moveX + moveZ;
+		rb.velocity = new Vector3(move.x, rb.velocity.y, move.z);
+		//rb.velocity = new Vector3(inputs.movement.x * moveSpeed, rb.velocity.y, inputs.movement.y * moveSpeed); //OLD VERSION WITHOUT FORWARD
 	}
 
 	private void OnDestroy()
